a20de433b4aed13fae09db6d40c8714dbdece9f1
Sharmin <sharmin@Sharmins-MacBook-Pro.local>
2019-05-11 13:10:05 +0600

    first php basic code

---
+++ hello-world.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?= "hello world"; ?></title>
+</head>
+<body>
+<?php
+echo "<h1>Hello world</h1>";
+echo "Learning 'PHP' coding";
+echo "<br>";
+echo 'Learning "PHP" coding';
+echo "<br>";
+echo strrev('Learning "PHP" coding');
+echo "<br>";
+echo strtoupper('Learning "PHP" coding');
+echo "<br>";
+echo strtolower('Learning "PHP" coding');
+
+?>
+
+</body>
+</html>
