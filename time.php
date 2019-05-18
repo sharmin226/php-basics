6f6f074146d5171515b0cb8be82215745439d004
Sharmin <sharmin@Sharmins-MacBook-Pro.local>
2019-05-19 00:52:48 +0600

    new addition in PHP

---
+++ time.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+		echo "The time now is ".Date("h:i:s A");
+	//d M Y-> H:i:s
+	//A-> Uppercase Ante meridiem and Post meridiem	AM or PM
+		echo '<br/>Don\'t rush to learn anything new.';
+		//OR
+		echo "<br/>Don't panic for future life.";
+
+	?>
+
+</body>
+</html>
